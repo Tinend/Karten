84ba72a4e97aedd1aeeb119b3cbc0eae74e52d20
Tinend <ulrich.brodowsky@gmx.net>
2015-07-09 16:52:14 +0200

    Bruteforce eingebaut

---
+++ entscheider/Bruteforce.rb
@@ -0,0 +1,19 @@
+module Bruteforce
+
+  def brute()
+    handkarten = @wisser.eigener_stapel.hand.length
+    laenge = @wisser.eigener_stapel.laenge + 2
+    maxwahl = [-1, -1, -1]
+    max = bewerte(maxwahl)
+    (laenge ** handkarten).times do |wahlzahl|
+      wahl = Array.new(handkarten) {|i| (wahlzahl / (laenge ** i) % laenge) - 2}
+      wahl = zurechtschneiden(wahl, laenge, @wisser.eigener_stapel.hand.length)
+      bew = bewerte(wahl)
+      if (bew <=> max) == 1
+        max = bew
+        maxwahl = wahl
+      end
+    end
+    maxwahl
+  end
+end

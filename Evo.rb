0ab0a706968cc345a79eddad8c8bcdb9cb3dcd22
Tinend <ulrich.brodowsky@gmx.net>
2015-07-20 13:50:24 +0200

    Mehr eingaben beim Evoprogramm

--- Evo.rb
+++ Evo.rb
@@ -8,16 +8,23 @@ require "Tichu.rb"
 require "Entscheider.rb"
 require "Entscheider_Merker.rb"
 
-epsilon = 0.3
-q = 0.77
-runden = 1
-anzahl = 11
-faktor = 1.15
-minepsilon = 0.025
+puts "Warnung, dieses Programm strüzt bei der Wahl der falschen Entscheider ab! Es funktioniert nur, falls die Entscheider die Wahl eines Parameters unterstützen."
+teste = entscheider_wahl
+puts "Wähle epsilon. (Empfohlen: 0.3)"
+epsilon = gets.to_f
+puts "Wähle Startwert. (Empfohlen: 0.8)"
+q = gets.to_f
+puts "Wähle die Anzahl der Runden. (Empfohlen: 1)"
+runden = gets.to_i
+puts "Wähle die Spielerzahl. (Empfohlen: 11)"
+anzahl = gets.to_i
+puts "Wähle den Faktor. (Empfohlen: 1.15) Warnug! Das Programm funktioniert nur mit Werten > 1!"
+faktor = gets.to_f
+puts "Wie genau soll der Wert ermittelt werden? (Empfohlen: 0.02)"
+minepsilon = gets.to_f
 kartenname, karten = tichu
 regeln = Regeln.new(karten, 3, 1, 0)
 spiele = anzahl * (anzahl - 1) * runden
-teste = entscheider_wahl
 until epsilon < minepsilon
   entscheider = Array.new(anzahl) {|i| teste.new(Eroberungswerter_n.new(epsilon * 2 * i / (anzahl - 1) - epsilon + q), i)}
   entscheider.shuffle!
